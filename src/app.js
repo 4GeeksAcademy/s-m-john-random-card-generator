c33b84a0d5f12efe35a25c6d12390ab5f58d59ea
s-m-john <107650644+s-m-john@users.noreply.github.com>
2023-07-11 06:49:35 +0000

    minor updates to June 9, 2023 Card Generator Project.

--- src/app.js
+++ src/app.js
@@ -3,23 +3,45 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
-  //write your code here
-  console.log("You see this message everytime the page loads!");
+  console.log("You see this message every time the page loads!");
 
   const suites = ["♥", "♠", "♦", "♣"];
 
+  const cardValues = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
   function randomCard(arr) {
-    return Math.floor(Math.random() * arr.length);
+    return arr[Math.floor(Math.random() * arr.length)];
   }
 
-  let suite = suites[randomCard(suites)];
+  let suite = randomCard(suites);
   console.log("This is the random suite", suite);
 
+  let value = randomCard(cardValues);
+  console.log("This is the random value", value);
+
   let topsuite = document.querySelector("#topsuite");
-  topsuite.innerHTML = suite;
+  topsuite.textContent = suite;
 
   let bottomsuite = document.querySelector("#bottomsuite");
-  bottomsuite.innerHTML = suite;
+  bottomsuite.textContent = suite;
 
-  // console.log("This is the random number", randomCard(suites));
+  let valueElement = document.querySelector(".number");
+  valueElement.textContent = value;
 };
+
+// console.log("This is the random number", randomCard(suites));
+//};
